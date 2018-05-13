244c6ae1beb4cd5495dc66f91519111430ef843a
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-12 22:52:38 -0500

    Create 2-4 First Expression.js

---
+++ Area 2- Web Programming with JavaScript/2-4 First Expression.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: First Expression (2-1)
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+// console.log(1 + 5 - 2);
+console.log(1 + 5 - 2 * 4 / 2 + 54 / 3 + 22);
