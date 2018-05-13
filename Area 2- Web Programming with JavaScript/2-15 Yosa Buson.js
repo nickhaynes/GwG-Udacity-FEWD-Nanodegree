dcea91f5194d7f149a416ce51ed175de651d3fbb
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-13 10:52:14 -0500

    Create 2-15 Yosa Buson.js

---
+++ Area 2- Web Programming with JavaScript/2-15 Yosa Buson.js
@@ -0,0 +1,7 @@
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+// var haiku = /* concatenate the strings here */
+var haiku = "Blowing from the west" + "\nFallen leaves gather" + "\nIn the east."
+console.log(haiku);
