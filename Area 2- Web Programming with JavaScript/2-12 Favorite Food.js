69217e3e7ad226e5c02afa1f7b92e0850785c9d4
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-12 23:32:01 -0500

    Create 2-12 Favorite Food.js

---
+++ Area 2- Web Programming with JavaScript/2-12 Favorite Food.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: Favorite Food (2-3)
+ */
+ favFood = "Flame-broiled steak"
+
+*/ Console should print either "BBQ chicken" or "flame-broiled steak".
+
+console.log(favFood);
