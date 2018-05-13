f65b4a8a4bdcc9af520b7183ffc848f2df982b68
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-13 11:07:32 -0500

    Create 2-20 Semicolons.js

---
+++ Area 2- Web Programming with JavaScript/2-20 Semicolons.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+
+// Directions:
+// Define two variables called thingOne and thingTwo and assign them 
+// values. Print the values of both variables in one console.log statement 
+// using concatenation. For example, red blue where "red" is the value of
+// thingOne and "blue" is the value of thing Two.  Don't forget to use
+// semicolons!
+
+// your code goes here
+
+var thingOne = "red";
+var thingTwo = "blue";
+
+console.log(thingOne + ' ' + thingTwo);
