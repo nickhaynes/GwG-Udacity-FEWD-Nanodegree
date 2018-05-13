a31cb3952fe39a1b7e2291ad25ca5d95425e2f25
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-13 10:45:52 -0500

    Create 2-13 String Equality for All.js

---
+++ Area 2- Web Programming with JavaScript/2-13 String Equality for All.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: String Equality for All (2-4)
+ */
+
+// fix the right side of the expression
+// here is the original that I had to fix: 
+// var answer = "ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal";
+
+ var answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal";
+console.log(answer);
