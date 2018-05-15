e0ac790d7d49b9cf6e0f65bdc3842fe9ea34daf3
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-14 19:20:47 -0500

    Create 4-14 Factorials.js

---
+++ Area 2- Web Programming with JavaScript/Lesson 4- Loops/4-14 Factorials.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+ 
+// Write a for loop that prints out the factorial of the number 12
+var solution = 1;
+
+for (var x = 12; x > 0; x--) {
+    solution *= x;
+}
+
+console.log(solution);
