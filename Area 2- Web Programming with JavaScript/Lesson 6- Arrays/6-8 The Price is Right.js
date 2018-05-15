f1516473b82314a00378abcf757830f6dfda7471
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-15 08:32:34 -0500

    Create 6-8 The Price is Right.js

---
+++ Area 2- Web Programming with JavaScript/Lesson 6- Arrays/6-8 The Price is Right.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+ 
+ //Directions:
+// Starting with this array of prices, change 
+// the prices of the 1st, 3rd, and 7th elements 
+// in the array.  Afterwards, print out the prices
+// array to the console.
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+
+prices[0] = "8.42";
+prices[2] = "24.82";
+prices[6] = "19.84";
+
+console.log(prices);
