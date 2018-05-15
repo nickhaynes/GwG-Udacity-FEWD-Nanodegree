026dd5ab88fa1fd17176e8fd2327d13548067e70
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-15 08:30:07 -0500

    Create 6-7 Building the Crew.js

---
+++ Area 2- Web Programming with JavaScript/Lesson 6- Arrays/6-7 Building the Crew.js
@@ -0,0 +1,23 @@
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+
+// Directions:
+// Create an array called crew to organize the 
+// Serenity’s crew and set it equal to the 
+// variables below . You don't need to type out 
+// the actual strings, just use the provided variables.
+// 
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// your code goes here
+
+crew = ['Mal', 'Zoe', 'Wash', 'Inara', 'Jayne', 'Kaylee'];
+
+console.log(crew);
