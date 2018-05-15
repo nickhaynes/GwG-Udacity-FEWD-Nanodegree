0644c74231164bb1eff585da37800ea900b84344
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-14 19:57:45 -0500

    Create 5-5 Laugh it Off 1.js

---
+++ Area 2- Web Programming with JavaScript/Lesson 5- Functions/5-5 Laugh it Off 1.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+
+function laugh () {
+    giggles = "hahahahahahahahahaha!";
+    return giggles;
+}
+
+console.log(laugh());
