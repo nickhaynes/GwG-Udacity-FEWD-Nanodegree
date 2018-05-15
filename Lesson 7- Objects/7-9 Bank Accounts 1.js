f23f8d89a559f0c523a0cdf085a41ced59d242f9
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-15 16:51:45 -0500

    Create 7-9 Bank Accounts 1.js

---
+++ Lesson 7- Objects/7-9 Bank Accounts 1.js
@@ -0,0 +1,35 @@
+/*
+ * Programming Quiz: Bank Accounts 1 (7-3)
+ */
+
+
+// Directions:
+// Using the given object (prior to "your code goes here"),
+// add a printAccountSummary() that returns the following
+// account message:
+//
+// Welcome!
+// Your balance is currently $1000 and your interest rate is 1%.
+//
+
+var savingsAccount = {
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    },
+// your code goes here
+    printAccountSummary: function () {
+        return 'Welcome!\nYour balance is currently $' + savingsAccount.balance + ' and your interest rate is ' + savingsAccount.interestRatePercent + '%.';
+    }
+};
+
+console.log(savingsAccount.printAccountSummary());
