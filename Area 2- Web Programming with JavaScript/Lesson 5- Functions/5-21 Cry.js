47ce03f881537ac979a9fb3cf51332e34aa90ef7
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-14 21:40:10 -0500

    Create 5-21 Cry.js

---
+++ Area 2- Web Programming with JavaScript/Lesson 5- Functions/5-21 Cry.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Cry (5-5)
+ */
+
+// Directions:
+// Write a named function expression that stores 
+// the function in a variable called cry and 
+// returns "boohoo!". Don't forget to call the 
+// function using the variable name, not the 
+// function name:
+
+
+// your code goes here
+
+var cry = function tears() {
+    return 'boohoo!';
+};
+
+console.log(cry());
