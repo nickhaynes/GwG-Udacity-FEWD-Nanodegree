5e906374111a6e08e0e0e35712cb7e2d9e0a7861
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-15 16:33:53 -0500

    Create 7-3 Umbrella.js

---
+++ Lesson 7- Objects/7-3 Umbrella.js
@@ -0,0 +1,33 @@
+/*
+ * Programming Quiz: Umbrella (7-1)
+ */
+
+
+// Directions:
+// Using the umbrella example from the previous 
+// video, see if you can follow the example open() 
+// method and create the close() method. It's 
+// alright if you have trouble at first! We'll go 
+// into more detail later in this lesson.
+
+var umbrella = {
+    color: "pink",
+    isOpen: true,
+    open: function() {
+        if (umbrella.isOpen === true) {
+            return "The umbrella is already opened!";
+        } else {
+            umbrella.isOpen = true;
+            return "Julia opens the umbrella!";
+        }
+    },
+
+// your code goes here
+    close: function() {
+        if (umbrella.isOpen === false) {
+            return "The umbrella is already closed!";
+        } else {
+            umbrella.isOpen = false;
+            return "Julia closes the umbrella!";
+        } 
+}};
