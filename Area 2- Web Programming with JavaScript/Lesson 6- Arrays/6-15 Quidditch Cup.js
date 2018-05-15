d98e1f9daef366699e6d1cd0fe7a0d5fd08298e7
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-15 10:55:16 -0500

    Create 6-15 Quidditch Cup.js

---
+++ Area 2- Web Programming with JavaScript/Lesson 6- Arrays/6-15 Quidditch Cup.js
@@ -0,0 +1,22 @@
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ */
+// Directions:
+// Create a function called hasEnoughPlayers() 
+// that takes the team array as an argument 
+// and returns true or false depending on if 
+// the array has at least seven players.
+
+// your code goes here
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+
+function hasEnoughPlayers(team) {
+    if (team.length >= 7) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(hasEnoughPlayers(team));
