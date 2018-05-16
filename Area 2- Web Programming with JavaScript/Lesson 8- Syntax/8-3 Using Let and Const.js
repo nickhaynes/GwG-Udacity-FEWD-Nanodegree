96a2fb6943e3039fc62ee63a9b764165c0667f27
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-16 04:11:11 -0500

    Create 8-3 Using Let and Const.js

---
+++ Area 2- Web Programming with JavaScript/Lesson 8- Syntax/8-3 Using Let and Const.js
@@ -0,0 +1,25 @@
+/*
+ * Programming Quiz: Using Let and Const (1-1)
+ */
+//
+// DIRECTIONS:
+//
+// Replace the variable declarations using let or const.
+//
+// NOTE: All variable declarations in code given were using var.
+//
+const CHARACTER_LIMIT = 255;
+const posts = [
+	"#DeepLearning transforms everything from self-driving cars to language translations. AND it's our new Nanodegree!",
+	"Within your first week of the VR Developer Nanodegree Program, you'll make your own virtual reality app",
+	"I just finished @udacity's Front-End Web Developer Nanodegree. Check it out!"
+];
+
+// prints posts to the console
+function displayPosts() {
+	for (let i = 0; i < posts.length; i++) {
+		console.log(posts[i].slice(0, CHARACTER_LIMIT));
+	}
+}
+
+displayPosts();
