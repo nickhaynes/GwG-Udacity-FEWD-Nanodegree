b55641394dcc4d771332967770a2492d6b3c6082
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-05-16 08:16:18 -0500

    Create 8-4 Template Literals.js

---
+++ Area 2- Web Programming with JavaScript/Lesson 8- Syntax/8-4 Template Literals.js
@@ -0,0 +1,15 @@
+/*
+ * Instructions: Change the `greeting` string to use a template literal.
+ */
+// OLD WAY:
+// const myName = '[NAME]';
+// const greeting = 'Hello, my name is ' + myName;
+// console.log(greeting);
+//
+
+// your code goes here
+
+const myName = 'Nick';
+const greeting = `Hello, my name is ${myName}.`;
+console.log(greeting);
+
