a13abc0c7855271e7b28ba37af45276a8dae0cb9
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-06-11 04:53:11 -0500

    Create script.js

---
+++ Area 3- Exploring JS- Objects- Tools and Testing/Lesson 10-Functions/10-11 Using Default Function Parameters/script.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Using Default Function Parameters (2-2)
+ */
+/* YOUR CODE GOES HERE */
+function buildHouse({floors = 1, color = 'red', walls = 'brick'} = {}) {
+    return `Your house has ${floors} floor(s) with ${color} ${walls} walls.`;
+}
+
+/* tests
+console.log(buildHouse()); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({})); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({floors: 3, color: 'yellow'})); // Your house has 3 floor(s) with yellow brick walls.
+*/
